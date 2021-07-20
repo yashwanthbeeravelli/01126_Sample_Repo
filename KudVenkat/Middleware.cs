f1585b65b6017b8a07b15831fe65a18ae2c80605
Yashwanth <yashwanthbeeravelli@gmail.com>
2021-07-20 16:19:23 -0400

    launchsettings.json, Middleware, Configuring_PipeLine, Middleware, DeveloperExceptionPage

---
+++ KudVenkat/Middleware.cs
@@ -0,0 +1,45 @@
+﻿using Microsoft.AspNetCore.Builder;
+using Microsoft.AspNetCore.Http;
+using Microsoft.Extensions.Logging;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace KudVenkat
+{
+    // You may need to install the Microsoft.AspNetCore.Http.Abstractions package into your project
+    public class Middleware
+    {
+        private readonly RequestDelegate _next;
+        private readonly ILogger _logger;
+        
+        public Middleware(RequestDelegate next, ILoggerFactory logFactory)
+        {
+            _next = next;
+            _logger = logFactory.CreateLogger("My Middleware");
+        }
+
+        /* A Middleware needs to have either Invoke or InvokeAsync methods*/
+        public Task Invoke(HttpContext httpContext)
+        {
+            return _next(httpContext);
+        }
+
+        /*
+        public async Task InvokeAsync(HttpContext context)
+        {
+            await _next(context);
+        }
+        */
+    }
+
+    // Extension method used to add the middleware to the HTTP request pipeline.
+    public static class MiddlewareExtensions
+    {
+        public static IApplicationBuilder UseMiddleware(this IApplicationBuilder builder)
+        {
+            return builder.UseMiddleware<Middleware>();
+        }
+    }
+}
---
+++ KudVenkat/Startup.cs
@@ -0,0 +1,99 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Builder;
+using Microsoft.AspNetCore.Hosting;
+using Microsoft.AspNetCore.Http;
+using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.DependencyInjection;
+using Microsoft.Extensions.Hosting;
+using Microsoft.Extensions.Logging;
+
+namespace KudVenkat
+{
+    public class Startup
+    {
+        private IConfiguration _configuration;
+
+        // This method gets called by the runtime. Use this method to add services to the container.
+        // For more information on how to configure your application, visit https://go.microsoft.com/fwlink/?LinkID=398940
+        public void ConfigureServices(IServiceCollection services)
+        {
+
+        }
+
+        public Startup(IConfiguration configuration)
+        {
+            _configuration = configuration;
+        }
+
+        // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
+        public void Configure(IApplicationBuilder app, IWebHostEnvironment env, ILogger<Startup> logger)
+        {
+
+            if (env.IsDevelopment())
+            {
+                DeveloperExceptionPageOptions developerExceptionPageOptions = new DeveloperExceptionPageOptions {
+                    SourceCodeLineCount = 10
+                };
+                app.UseDeveloperExceptionPage(developerExceptionPageOptions);
+            }
+
+            DefaultFilesOptions defaultFileOptions = new DefaultFilesOptions();
+            defaultFileOptions.DefaultFileNames.Clear();
+            defaultFileOptions.DefaultFileNames.Add("foo.html");
+
+            app.UseDefaultFiles(defaultFileOptions);
+            app.UseStaticFiles();
+            app.UseMiddleware<Middleware>();
+
+            app.UseRouting();
+            app.UseEndpoints(endpoints =>
+            {
+                endpoints.MapGet("/", async context =>
+                {                    
+                    //await context.Response.WriteAsync(_configuration["MyKey"]);
+                    await context.Response.WriteAsync("Hello World!");
+                    //await context.Response.WriteAsync(System.Diagnostics.Process.GetCurrentProcess().ProcessName);
+                });
+            });
+            
+            //app.UseWelcomePage();
+            app.Use(async (context, next) => 
+            {
+                logger.LogInformation("MW1: Incoming Request");
+                await next();
+                logger.LogInformation("MW1: Outgoing Response");
+            }
+            );
+
+            app.Use(async (context, next) =>
+            {
+                logger.LogInformation("MW2: Incoming Request");
+                await next();
+                logger.LogInformation("MW2: Outgoing Response");
+            });
+            //app.Use(async (context, next) => { await context.Response.WriteAsync("Hello Yashwanth. This is 1st Middleware"); await next(); });
+            app.Run(async (context) => {
+                throw new Exception("Some error while processing ");
+                await context.Response.WriteAsync("Yoo Bro. This is the Terminal Middleware."); 
+            });
+            app.UseStaticFiles();
+
+            app.Use(async (context, next) =>
+            {
+                await context.Response.WriteAsync("Hi Yashwanth. This is 1st MiddleWare");
+                await next();
+            });
+
+            app.Run(async (context) =>
+            {
+                await context.Response.WriteAsync("Hi Yashwanth. This is 2nd MiddleWare");
+            });
+            
+            app.UseStaticFiles();
+            
+        }
+    }
+}
