0df69908bf3a94e00f922ab913c9117b29f9447f
Yashwanth <yashwanthbeeravelli@gmail.com>
2021-07-23 21:29:45 -0400

    Video 23: How to pass the data from a Controller to a View using ViewData

--- KudVenkat/Controllers/HomeController.cs
+++ KudVenkat/Controllers/HomeController.cs
@@ -15,34 +15,19 @@ namespace KudVenkat.Controllers
         {
             _employeeRepository = employeeRepository;
         }
-        /*
+        
         public string Index()
         {
-            return _employeeRepository.GetEmployeeById(2).Name;
-            //return "Hello World. This is from MVC HomeController";
+            return _employeeRepository.GetEmployeeById(2).Name;        
         }
-        */
-        public ViewResult Index()
+            
+        /* This function returns the data in ViewData */
+        public ViewResult Details()
         {
-            return View(); //Will call the Views/Home/Index.cshtml
-            return View("Details"); //Will call the Views/Home/Details.cshtml
-            return View("../Test/TestDetails"); //Will call the Views/Test/Details.cshtml            
-            //return View("This should post the view on Home/Index.cshtml");
-        }
-
-
-        /* The below code returns the result in a Json Format. Typically used for Web APIs, REST Services */
-        public JsonResult JsonDetails()
-        {
-            Employee model = _employeeRepository.GetEmployeeById(3);
-            return Json(model);
-        }
-
-        /* The below code returns the result in a XML Format. */
-        public ObjectResult ObjDetails()
-        {
-            Employee model = _employeeRepository.GetEmployeeById(3);
-            return new ObjectResult(model);
+            Employee employee = _employeeRepository.GetEmployeeById(1);
+            ViewData["Employee"] = employee;
+            ViewData["PageTitle"] = "Employee Info from ViewData";
+            return View(employee);
         }
     }
 }
